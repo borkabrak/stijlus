3616a9daff7113d5f4fdd588a10cda205a880927
Jonathan Carter <jcarter@str.com>
2013-07-02 10:33:00 -0500

    Click to place selected shape

--- draw.js
+++ draw.js
@@ -1,11 +1,6 @@
 $(function(){
 
-    var cursor = {
-        x: 0,
-        y: 0,
-    };
-
-    paper = new Raphael(document.getElementById("svg-container"), 400, 500);
+    paper = new Raphael(document.getElementById("svg-container"), 600, 500);
 
     $("#x, #y, #x-radius, #y-radius, #width, #height").prop("min","0").prop("step","20");
     $("#y").prop("max",$("svg").prop("height"));
@@ -15,32 +10,27 @@ $(function(){
         paper.clear();    
     });
 
-    $("button#circle").on('click',function(event){
-        circle = paper.circle( $("#x").val(), $("#y").val(), $("#x-radius").val() );
-    });
-
-    $("button#ellipse").on('click',function(event){
-        circle = paper.ellipse( $("#x").val(), $("#y").val(), $("#x-radius").val(), $("#y-radius").val() );
-    });
-
-    $("button#rectangle").on('click',function(event){
-        circle = paper.rect( $("#x").val(), $("#y").val(), $("#width").val(), $("#height").val() );
-    });
-
-    // Record last click
     $("svg").on("click", function(event){
 
         // Set the x,y coords to the click
         $("#x").val(event.offsetX);
         $("#y").val(event.offsetY);
+
+        var brush = ($("input[name=brush]:checked").val())
+
+        if (brush === "circle"){
+            brush = paper.circle( $("#x").val(), $("#y").val(), $("#x-radius").val() );
+        } else if (brush === "ellipse"){
+            brush = paper.ellipse( $("#x").val(), $("#y").val(), $("#x-radius").val(), $("#y-radius").val() );
+        } else if (brush === "rectangle"){
+            brush = paper.rect( $("#x").val(), $("#y").val(), $("#width").val(), $("#height").val() );
+        } else {
+            console.log("Unrecognized brush type '%s'",brush);
+        };
     });
 
     move_cursor = function(x, y){
         
-        // Paths.clear( Path.cursor(cursor.x,cursor.y,10) )
-        cursor.x = x;
-        cursor.y = y;
-
         paper.path( Paths.cursor(x, y, 10) );
     };
 
