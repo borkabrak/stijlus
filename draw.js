5aa65a6f75959da4811f8459bd80e300de20fb6d
Jonathan Carter <jcarter@str.com>
2013-07-02 11:50:22 -0500

    Line drawing!

--- draw.js
+++ draw.js
@@ -2,28 +2,41 @@ $(function(){
 
     paper = new Raphael(document.getElementById("svg-container"), 600, 500);
 
+    var last_action;
+
     var brushes = {
-        'circle': function(x,y){
+        'circle': function(x, y){
             return paper.circle( x, y, $("#x-radius").val() );
         },
 
-        'ellipse': function(x,y){
+        'ellipse': function(x, y){
             return paper.ellipse( x, y, $("#x-radius").val(), $("#y-radius").val() );
         },
 
-        'rectangle': function(x,y){
+        'rectangle': function(x, y){
             return paper.rect( x, y, $("#width").val(), $("#height").val() );
+        },
+
+        'line': function(x, y){
+            return { x: x, y: y };
         }
     };
 
-    $("#x-radius, #y-radius, #width, #height").prop("min","0").prop("step","20");
+    $("#x-radius, #y-radius, #width, #height").prop("min", "0").prop("step", "20");
 
-    $("button#clear").on('click',function(event){
+    $("button#clear").on('click', function(event){
         paper.clear();    
     });
 
-    $("svg").on("click", function(event){
-        var shape = brushes[$("input[name=brush]:checked").val()](event.offsetX, event.offsetY);
+    $("svg").on("mousedown", function(event){
+        last_action = brushes[$("input[name=brush]:checked").val()](event.offsetX, event.offsetY);
+    });
+
+    $("svg").on("mouseup", function(event){
+        if ($("input[name=brush]:checked").val() === 'line'){
+            paper.path("M " + last_action.x + " " + last_action.y +
+            "L " + event.offsetX + " " + event.offsetY);
+        };
     });
 
 });
