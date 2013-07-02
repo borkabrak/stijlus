e74edd5f92dec653714a9086af034c2fd041e235
Jonathan Carter <jcarter@str.com>
2013-07-02 10:47:06 -0500

    X and Y inputs removed.  These are best gleaned from a mouse click.

--- draw.js
+++ draw.js
@@ -2,50 +2,45 @@ $(function(){
 
     paper = new Raphael(document.getElementById("svg-container"), 600, 500);
 
-    $("#x, #y, #x-radius, #y-radius, #width, #height").prop("min","0").prop("step","20");
-    $("#y").prop("max",$("svg").prop("height"));
-    $("#x").prop("max",$("svg").prop("width" ));
+    var brushes = {
+        'circle': function(x,y){
+            return paper.circle( x, y, $("#x-radius").val() );
+        },
+
+        'ellipse': function(x,y){
+            return paper.ellipse( x, y, $("#x-radius").val(), $("#y-radius").val() );
+        },
+
+        'rectangle': function(x,y){
+            return paper.rect( x, y, $("#width").val(), $("#height").val() );
+        }
+    };
+
+    $("#x-radius, #y-radius, #width, #height").prop("min","0").prop("step","20");
 
     $("button#clear").on('click',function(event){
         paper.clear();    
     });
 
     $("svg").on("click", function(event){
-
-        // Set the x,y coords to the click
-        $("#x").val(event.offsetX);
-        $("#y").val(event.offsetY);
-
-        var brush = ($("input[name=brush]:checked").val())
-
-        if (brush === "circle"){
-            brush = paper.circle( $("#x").val(), $("#y").val(), $("#x-radius").val() );
-        } else if (brush === "ellipse"){
-            brush = paper.ellipse( $("#x").val(), $("#y").val(), $("#x-radius").val(), $("#y-radius").val() );
-        } else if (brush === "rectangle"){
-            brush = paper.rect( $("#x").val(), $("#y").val(), $("#width").val(), $("#height").val() );
-        } else {
-            console.log("Unrecognized brush type '%s'",brush);
-        };
+        var shape = brushes[$("input[name=brush]:checked").val()](event.offsetX, event.offsetY);
     });
 
     move_cursor = function(x, y){
-        
         paper.path( Paths.cursor(x, y, 10) );
     };
 
     /* A simple namespace for building various path strings. */
     Paths = {
-
         cursor:  function(x,y,size){
             // Return a Path string drawing a cross-shaped cursor, centered on
             // the given coordinates.
+
             return "M" + x + " " + y +
             "m 0" + (-1 * size) +
             "v" + (size * 2) + 
             "m " + (-1 * size) + " " + (-1 * size) +
             "h" + (size * 2); },
-
     };
 });
 
